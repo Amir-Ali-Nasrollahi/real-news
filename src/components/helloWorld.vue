113ddfed98da9d6da73e0e661d0c32c70d46fd50
amirali <nasrollahiamirali@gmail.com>
2024-07-30 11:49:23 +0330

    firstcommit

---
+++ src/components/helloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>{{ hello_world }}</div>
+</template>
+
+<script setup>
+const hello_world = 'welcome to the first page';
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from '@/components/helloWorld.vue';
+
+import { reactive } from 'vue'
+
+const hello = HelloWorld;
+const welcome = reactive('welcome to the home page on this website')
+
+</script>
+
+<template>
+  <main>
+    {{ welcome }}
+    <hello class="hello" />
+  </main>
+</template>
+
+<style>
+.hello {
+  background-color: aqua;
+  color: black;
+  padding: 10px;
+  font-size: 30px;
+}
+</style>
